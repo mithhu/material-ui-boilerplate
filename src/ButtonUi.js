58a8d919a8bfa4d98856156a9ef62f7a504deff0
Maliha <malihakabir01@gmail.com>
2019-08-04 13:53:33 +0600

    boilerplate

---
+++ src/ButtonUi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './ButtonUi.css';
+
+export default function ButtonUi() {
+    return (
+        <div>
+            <Button className="button">Button</Button>
+        </div>
+    )
+}
